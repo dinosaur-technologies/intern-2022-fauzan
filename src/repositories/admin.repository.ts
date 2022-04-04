07d4abaa0973a481713390990f2d6e0c0f47037f
Deojeff Lai <d30jeff@gmail.com>
2022-04-04 09:13:10 +0800

    Add Repository

---
+++ src/repositories/admin.repository.ts
@@ -0,0 +1,43 @@
+import { PrismaClient, Prisma, Admin } from '@prisma/client';
+const prisma = new PrismaClient();
+
+export class AdminsRepository {
+  async create(username: string, password: string, email: string) {
+    const newAdmin = await prisma.admin.create({
+      data: {
+        username,
+        password,
+        email,
+      },
+    });
+    console.log('New admin: ');
+    console.log(newAdmin);
+  }
+
+  async list() {
+    const listAdmin = await prisma.admin.findMany();
+
+    console.log('List admin: ');
+    console.log(listAdmin);
+  }
+
+  async deleteById(id: number) {
+    const deleteAdmin = await prisma.admin.delete({
+      where: {
+        id,
+      },
+    });
+    console.log('Delete admin: ');
+    console.log(deleteAdmin);
+  }
+
+  async findOnebyId(id: number) {
+    const findAdmin = await prisma.admin.findUnique({
+      where: {
+        id,
+      },
+    });
+    console.log('The admin that had been found: ');
+    console.log(findAdmin);
+  }
+}
