0904aea43bf35818f3bddd3a97a42287c71bc753
FZan99 <fauzan.jeff@gmail.com>
2022-08-08 09:17:58 +0800

    Create middleware to authenticate user

---
+++ src/middlewares/is-authenticated-user.middleware.ts
@@ -0,0 +1,21 @@
+import { Middleware } from '@decorators/express';
+import { UnauthorizedException } from '@exceptions/http-exception';
+import { repositories } from '@repositories/index.repository';
+import { services } from '@services/index.service';
+import { NextFunction, Request, Response } from 'express';
+
+export class IsAuthenticatedUser implements Middleware {
+  public async use(req: Request, res: Response, next: NextFunction) {
+    if (!req.session.account) {
+      return next(new UnauthorizedException('Invalid Credentials'));
+    }
+
+    const account = await repositories.users.findOnebyId(req.session.account.id);
+
+    if (!account) {
+      return next(new UnauthorizedException('Unauthorized'));
+    }
+
+    next();
+  }
+}
